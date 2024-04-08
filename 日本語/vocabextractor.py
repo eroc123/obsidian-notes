76bf8d4eba3bc77b4d9491f7a5d3a2bb522d8189
alex <alexsundbergcnx@gmail.com>
2024-04-08 14:28:05 +0700

    vault backup: 2024-04-08 14:28:05

--- 日本語/vocabextractor.py
+++ 日本語/vocabextractor.py
@@ -1,2 +1,15 @@
 import requests
-import pdfkit
+from bs4 import BeautifulSoup
+
+def download_article_as_pdf(url, output_path):
+    #fetch html content of article
+    response = requests.get(url)
+    html_content=response.text
+    # Convert HTML content to PDF
+    print(response.text)
+
+# Example usage
+url = "https://www3.nhk.or.jp/news/easy/k10014413031000/k10014413031000.html"
+output_path = "article.pdf"
+
+download_article_as_pdf(url, output_path)
