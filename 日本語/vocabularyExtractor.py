f74b7c757370a8ab0a34ad34b865e78b927b1358
alex <alexsundbergcnx@gmail.com>
2024-04-12 00:18:08 +0700

    vault backup: 2024-04-12 00:18:08

--- 日本語/vocabularyExtractor.py
+++ 日本語/vocabularyExtractor.py
@@ -1,12 +1,12 @@
 from sudachipy import tokenizer
 from sudachipy import dictionary
-
-# Load Sudachi dictionary
-sudachi = dictionary.Dictionary()
-
 # Tokenize Japanese text
 mode = tokenizer.Tokenizer.SplitMode.C
-tokens = sudachi.tokenize("彼は東京へ行った。そして、美味しい寿司を食べた。", mode)
+# Load Sudachi dictionary
+sudachi = dictionary.Dictionary().create(mode)
+
+
+tokens = sudachi.tokenize("彼は東京へ行ったそして美味しい寿司を食べた。", mode)
 
 # Extract vocabulary
 vocabulary = [token.surface() for token in tokens]
