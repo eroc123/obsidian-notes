ed19dd0a4c23fe0cde36f1874b429cc37f9144e8
alex <alexsundbergcnx@gmail.com>
2024-04-15 10:20:53 +0700

    vault backup: 2024-04-15 10:20:52

--- 日本語/simulation2.py
+++ 日本語/simulation2.py
@@ -1,63 +1,19 @@
-import random, numpy as np, math
-from sympy import isprime
-def list_factors(n):
-    factors = set()
-    for i in range(1, int(np.sqrt(n)) + 1):
-        if n % i == 0:
-            factors.add(i)
-            factors.add(n // i)
-    return sorted(factors)
+import math
+class vector:
+    def __init__(self, x,y,z, magnitude):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.magnitude = magnitude
 
-
-
-print(list_factors(12))
-
-
-def game(number):
-    choices = []
-    factors = list_factors(number)
-    gameEnd = False
-    #emma does the first round and selects an element
-    #1 is emma, 2 is nelly
-    chosenNumber = random.choice(factors)
-    factors.remove(chosenNumber)
-    playerTurn = 'Nelly'
-    while not gameEnd:
-        choices = []
-        if len(factors) == 0:
-            gameEnd = True
-            break
-        for element in factors:
-            if math.gcd(chosenNumber, element) == 1:
-                choices.append(element)
-        if len(choices) == 0:
-            gameEnd = True
-            break
-        chosenNumber = random.choice(choices)
-        factors.remove(chosenNumber)
-        if playerTurn == 'Nelly':
-            playerTurn='Emma'
-        else:
-             playerTurn = 'Nelly'
-    return playerTurn
-
-def checkPrimes(factors):
-    primeCount = 0
-    for i in factors:
-        if isprime(i):
-            primeCount+=1
-    return primeCount
-
-count = 0
-for i in range(2, 100):
-    factors = list_factors(i)
-    if len(factors) > 6:
-        count += 1
-        print(i, ": ", factors)
-print(count)
-factors100 = []
-for i in range(100):
-    factors100.append(i)
-print(checkPrimes(factors100))
-
-factors = print(list_factors(2002), ": factors of 2002", checkPrimes(list_factors(2002)))
+vectorsToDefine = ["AB", "AC", "AD", "BC", "BD", "CD"]
+vectors = {
+}
+def generateShape():
+    for i in vectorsToDefine:
+        if i == "AB" or "CD" == i:
+            vectors[i] = vector(None, None, None, 4)
+        elif i =="AC" or "BD" == i:
+            vectors[i] == vector(None, None, None, 5)
+        elif i =="AD" or "BC" == i:
+            vectors[i] == vector(None, None, None, 6)
